df6f11a40b9e6f4497d72a5ec3aecbc170cb5491
Rex Williams <degenincel070@gmail.com>
2025-03-20 00:08:40 -0500

    removed apples

--- gore/sort.sh
+++ gore/sort.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while : ; do
     num=0
-    ls *.img | sort > /tmp/file.list
+    ls *.img | sort -r > /tmp/file.list
     while read -r infile; do
         export num="$(echo "$num+1" | bc)"
         test -f "$num.img" || mv "$infile" "$num.img"
