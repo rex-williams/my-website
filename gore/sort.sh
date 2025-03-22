d84f5583b4d66c5bc56a1d63f0486084728d1c58
Rex Williams <degenincel070@gmail.com>
2025-03-22 04:28:11 -0500

    removed gore

--- gore/sort.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-while : ; do
-    num=0
-    ls *.img | sort -r > /tmp/file.list
-    while read -r infile; do
-        export num="$(echo "$num+1" | bc)"
-        test -f "$num.img" || mv "$infile" "$num.img"
-    done</tmp/file.list
-done
